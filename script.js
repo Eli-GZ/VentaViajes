eea6c72b1d3c1820a36381e145f98a2b2d7011d7
Elias <elias.d.gonzalez25@gmail.com>
2024-03-20 18:17:47 -0300

    Base

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+import { barcelona, roma, paris, londres } from './ciudades.js';
 
+//Datos
+let enlaces = document.querySelectorAll('a');
+let tituloElemento = document.getElementById('titulo');
+let subTituloElemento = document.getElementById('subtitulo');
+let parrafoElemento = document.getElementById('parrafo');
+let precioElemento = document.getElementById('precio')
+
+
+//evento click
+
+enlaces.forEach(function (enlace) {
+  enlace.addEventListener('click', function () {
+    //Remover clase active
+    enlaces.forEach(function (enlace) {
+      enlace.classList.remove('active');
+    });
+    //AGREGAR ACTIVE
+    this.classList.add('active');
+
+    //MOSTRAR CONTENIDO
+    let contenido = obtenerContenido(this.textContent);
+
+    //ESCRIBIR EL CONTENIDO
+    tituloElemento.innerHTML = contenido.titulo;
+    subTituloElemento.innerHTML = contenido.subtitulo;
+    parrafoElemento.innerHTML = contenido.parrafo;
+    precioElemento.innerHTML = contenido.precio;
+  });
+});
+
+//Función de información correcta
+
+function obtenerContenido(enlace) {
+  let contenido = {
+    'Barcelona': barcelona,
+    'Roma': roma,
+    'París': paris,
+    'Londres': londres
+  }
+  return contenido[enlace];
+}
